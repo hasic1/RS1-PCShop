510fa44b2ac9c47cc569f4f5d833cbca2bebea29
Armin Idriz <armin.idriz@edu.fit.ba>
2021-11-29 14:40:36 +0100

    PCShop

---
+++ PC_Shop/PC_Shop/Klase/Proizvodjac.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Shop.Klase
+{
+    public class Proizvodjac
+    {       
+      
+        public int ID { get; set; }
+        public string NazivProizvodjaca { get; set; }
+        public Drzava Sjediste { get; set; }
+        public int SjedisteID { get; set; }
+
+    }
+}
