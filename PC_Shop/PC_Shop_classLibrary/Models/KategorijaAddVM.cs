cf5839c79e22878c73540ef41f2f3fb00fa0229f
hase11 <bakir.hasic@edu.fit.ba>
2022-03-30 14:16:12 +0200

    Servisi

---
+++ PC_Shop/PC_Shop_classLibrary/Models/KategorijaAddVM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PC_Shop_classLibrary.Models
+{
+    public class KategorijaAddVM
+    {
+        public string NazivKategorije { get; set; }
+    }
+}
---
+++ PC_Shop/PC_Shop_classLibrary/Models/ProizvodjacAddVM.cs
@@ -0,0 +1,13 @@
+﻿using PC_Shop_classLibrary.Database;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PC_Shop_classLibrary.Models
+{
+    public class ProizvodjacAddVM
+    {
+        public string NazivProizvodjaca { get; set; }
+        public int SjedisteID { get; set; }
+    }
+}
